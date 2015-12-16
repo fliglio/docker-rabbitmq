71584b955866999d0f46c89759384a1a5c091b25
Ben Schwartz <benschw@gmail.com>
2015-12-16 13:24:30 -0600

    format updates for phinx cfg

--- phinx.php
+++ phinx.php
@@ -1,21 +1,19 @@
 <?php
 
-
-
-     return array(
-         "paths" => array(
-             "migrations" => getenv('PATH')
-         ),
-         "environments" => array(
-             "default_migration_table" => "phinxlog",
-             "default_database" => "dev",
-             "dev" => array(
-                 "adapter" => "mysql",
-                 "host" => getenv('DB_HOST'),
-                 "name" => getenv('DB_NAME'),
-                 "user" => getenv('DB_USER'),
-                 "pass" => getenv('DB_PASS'),
-                 "port" => getenv('DB_PORT')
-             )
-         )
-     );
+return [
+	"paths"        => [
+		"migrations" => getenv('PATH')
+	],
+	"environments" => [
+		"default_migration_table" => "phinxlog",
+		"default_database"        => "dev",
+		"dev"                     => [
+			"adapter" => "mysql",
+			"host"    => getenv('DB_HOST'),
+			"name"    => getenv('DB_NAME'),
+			"user"    => getenv('DB_USER'),
+			"pass"    => getenv('DB_PASS'),
+			"port"    => getenv('DB_PORT')
+		]
+	]
+];
